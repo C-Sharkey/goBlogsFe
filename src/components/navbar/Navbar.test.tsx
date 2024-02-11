62decb0ae249031e0ab90b399ac478763dc29191
C-Sharkey <colinsharkey88@hotmail.com>
2024-02-11 20:30:42 +0000

    Mantine nav added, tests updated

--- src/components/navbar/Navbar.test.tsx
+++ src/components/navbar/Navbar.test.tsx
@@ -1,29 +1,29 @@
 import { describe, expect, test } from 'vitest'
-import { render, screen } from "@testing-library/react";
-import Navbar from './Navbar'
+import { render, screen } from "../../test-utils";
+import { Navbar } from './Navbar';
 
 describe('Navbar', () => {
     test('Navbar is rendered', () => {
         const { container } = render(<Navbar />);
-        screen.debug();
         const navbar = container.querySelector("#navbar");
         expect(navbar).toBeDefined();
     })
     test('Logo section is rendered', () => {
         render(<Navbar />);
-        screen.debug();
-        expect(screen.getByText(/logoSection/i)).toBeDefined();
+        expect(screen.getByRole("link", {name: /blogs/i})).toBeDefined();
     })
     test('Nav section is rendered', () => {
         render(<Navbar />);
-        screen.debug();
         expect(screen.getByRole("navigation")).toBeDefined();
     })
     test('Sign in / sign up section is rendered', () => {
         render(<Navbar />);
-        screen.debug();
-        expect(screen.getAllByRole("button")).toHaveLength(2);
         expect(screen.getByRole("button", {name: /sign in/i})).toBeDefined();
         expect(screen.getByRole("button", {name: /sign up/i})).toBeDefined();
+    })
+    test('All nav buttons are rendered', () => {
+        render(<Navbar />);
+        expect(screen.getAllByRole("button")).toHaveLength(3);
+
     })
 })
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,23 +1,87 @@
-import { Button } from '@mantine/core';
-function Navbar() {
-        // navbar 
-            // logo section
-                // logo
-                // title
-                // link to home
-            // navigation section
-                // links
-            // login section
-                // 2 buttons
-  return (
-    <div id="navbar">
-        <div>LogoSection</div>
-        <nav>navSection</nav>
-        <div>
-            <Button>Sign in</Button>
-        </div>
-    </div>
-  )
-}
 
-export default Navbar
+import {
+    Group,
+    Button,
+    Divider,
+    Box,
+    Burger,
+    Drawer,
+    ScrollArea,
+    rem,
+    Text
+  } from '@mantine/core';
+  import { IconBrandGolang } from '@tabler/icons-react';
+  import { useDisclosure } from '@mantine/hooks';
+  import classes from './HeaderMegaMenu.module.css';
+  
+// Todo: fix css eslint error
+
+  export function Navbar() {
+    const [drawerOpened, { toggle: toggleDrawer, close: closeDrawer }] = useDisclosure(false);
+
+    return (
+      <Box pb={120}>
+        <header className={classes.header}>
+          <Group justify="space-between" h="100%">
+
+            <Box className={classes.logoSection}>
+              <a href="/">
+                <IconBrandGolang size={30} />
+                <Text fw={700}>Blogs</Text>
+              </a>
+            </Box>
+  
+            <Box component="nav">
+              <Group h="100%" gap={0} visibleFrom="sm">
+                <a href="#" className={classes.link}>
+                  About
+                </a>
+                <a href="#" className={classes.link}>
+                  Blog
+                </a>
+              </Group>
+            </Box>
+  
+            <Group visibleFrom="sm">
+              <Button variant="default">Sign in</Button>
+              <Button>Sign up</Button>
+            </Group>
+  
+            <Burger opened={drawerOpened} onClick={toggleDrawer} hiddenFrom="sm" />
+          </Group>
+        </header>
+  
+        <Drawer
+        opened={drawerOpened}
+        onClose={closeDrawer}
+        size="100%"
+        padding="md"
+        title="Navigation"
+        hiddenFrom="sm"
+        zIndex={1000000}
+      >
+        <ScrollArea h={`calc(100vh - ${rem(80)})`} mx="-md">
+          <Divider my="sm" />
+
+          <a href="#" className={classes.link}>
+            Home
+          </a>
+          <a href="#" className={classes.link}>
+            Learn
+          </a>
+          <a href="#" className={classes.link}>
+            Academy
+          </a>
+
+          <Divider my="sm" />
+
+          <Group justify="center" grow pb="xl" px="md">
+            <Button variant="default">Sign in</Button>
+            <Button>Sign up</Button>
+          </Group>
+        </ScrollArea>
+      </Drawer>
+
+      </Box>
+    );
+  }
